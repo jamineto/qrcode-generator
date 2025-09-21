9d3ba38cd0ae81fd8e2d84a4c9ab056fe676435d
User <ja1000.neto@hotmail.com>
2025-09-21 02:59:27 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    output: "export",
+    images: { unoptimized: true },
+    basePath: '/qrcode-generator',
+    assetPrefix: '/qrcode-generator/',
+    trailingSlash: true
+};
 
 export default nextConfig;
